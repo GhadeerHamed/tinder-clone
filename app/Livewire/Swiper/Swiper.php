7d74a3de0cf38e962b8384288e685c186f12ee17
Ghadeer <ghadeer.hamed@wesrom.com>
2024-05-30 05:17:24 +0300

    Design pages and swiper

---
+++ app/Livewire/Swiper/Swiper.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Livewire\Swiper;
+
+use Livewire\Component;
+
+class Swiper extends Component
+{
+    public function render()
+    {
+        return view('livewire.swiper.swiper');
+    }
+
+    public function swipedup()
+    {
+        dump("swipedup");
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,13 @@
 <?php
 
+use App\Livewire\Home;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+Route::get('app', Home::class)
+    ->middleware(['auth'])
+    ->name('app');
 
 Route::view('profile', 'profile')
     ->middleware(['auth'])
